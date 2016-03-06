651a5fbfbff1e8f5e8a8908bca9d8143ea3a8e9f
Mario Becerra <mariob.c@hotmail.com>
2016-03-05 19:42:23 -0600

    ejercicios libro, sum_array"

--- tarea2/script.js
+++ tarea2/script.js
@@ -1,7 +1,9 @@
 function skipSpace(string) {
-  var first = string.search(/\S/);
-  if (first == -1) return "";
-  return string.slice(first);
+  // El regex encuentra los espacios (incluyendo tabs y saltos de línea) y las líneas que tienen un # al principio
+  // match regresa un array con los elementos encontrados, nos quedamos con el primero y encontramos su longituud para
+  // con slice quedarnos con todo a partir del final del match
+  var ignora = string.match(/^([ \t\r\n]|\#.*)*/);
+  return string.slice(ignora[0].length);
 }
 
 //builds syntax tree
@@ -24,13 +26,30 @@ function parseApply(expr, program) {
   return parseApply(expr, program.slice(1));
 }
 
+// function parseExpression(program) {
+//   program = skipSpace(program);
+//   var match, expr;
+//   if (match = /^"([^"]*)"/.exec(program))
+//     expr = {type: "value", value: match[1]};
+//   else if (match = /^\d+\b/.exec(program))
+//     expr = {type: "value", value: Number(match[0])};
+//   else if (match = /^[^\s(),"]+/.exec(program))
+//     expr = {type: "word", name: match[0]};
+//   else
+//     throw new SyntaxError("Unexpected syntax: " + program);
+// 
+//   return parseApply(expr, program.slice(match[0].length));
+// }
+
 function parseExpression(program) {
   program = skipSpace(program);
   var match, expr;
   if (match = /^"([^"]*)"/.exec(program))
     expr = {type: "value", value: match[1]};
   else if (match = /^\d+\b/.exec(program))
-    expr = {type: "value", value: Number(match[0])};
+    expr = {type: "integer", value: Number(match[0])};
+  else if (match = /^\d+\b.\d+\b/.exec(program))
+    expr = {type: "float", value: Number(match[0])};  
   else if (match = /^[^\s(),"]+/.exec(program))
     expr = {type: "word", name: match[0]};
   else
@@ -38,6 +57,7 @@ function parseExpression(program) {
 
   return parseApply(expr, program.slice(match[0].length));
 }
+
 function parse(program) {
   var result = parseExpression(program);
   if (skipSpace(result.rest).length > 0)
@@ -47,7 +67,10 @@ function parse(program) {
 
 function evaluate(expr, env) {
   switch(expr.type) {
-    case "value":
+    case "integer":
+      return expr.value;
+
+    case "float":
       return expr.value;
 
     case "word":
@@ -123,29 +146,102 @@ specialForms["define"] = function(args, env) {
   return value;
 };
 
+// set: similar to define, which gives a variable a new value, updating the variable in an outer scope if it doesn’t already exist in the inner scope. 
+// If the variable is not defined at all, the function throws a ReferenceError.
+specialForms["set"] = function(args, env) {
+  if (args.length != 2 || args[0].type != "word")
+    throw new SyntaxError("Bad use of set");
+  var varName = args[0].name;
+  var value = evaluate(args[1], env);
+
+  for (var scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, varName)) {
+      scope[varName] = value;
+      return value;
+    }
+  }
+  throw new ReferenceError("Setting undefined variable " + varName);
+};
+
 // defines env
 var topEnv = Object.create(null);
+
 // boolean vals
 topEnv["true"] = true;
 topEnv["false"] = false;
+
 // other operands
 // <= and >= added
 ["+", "-", "*", "/", "==", "<", ">", "<=", ">="].forEach(function(op) {
   topEnv[op] = new Function("a, b", "return a " + op + " b;");
 });
+
 // print func
 topEnv["print"] = function(value) {
   console.log(value);
   return value;
 };
 
+// arrays
+// http://www.nfriedly.com/techblog/2009/06/advanced-javascript-objects-arrays-and-array-like-objects/
+// http://www.javascriptkit.com/javatutors/arrayprototypeslice.shtml
+topEnv["array"] = function() {
+   return Array.prototype.slice.call(arguments, 0);
+};
+
+// length of an array
+topEnv["length"] = function(array) {
+   return array.length;
+};
+
+// get the nth element of an array
+topEnv["element"] = function(array, position) {
+   return array[position];
+};
+
+// sum_array
+topEnv["sum_array"] = function(array) {
+  sum = array.reduce(function(a, b) { return a + b; }, 0);
+  return sum;
+};
+
+// sort_array
+topEnv["sum_array"] = function(array) {
+  sum = array.reduce(function(a, b) { return a + b; }, 0);
+  return sum;
+};
+
 // run program
+// The use of Array.prototype.slice.call is a trick to turn an array-like object, such as arguments, into a real array so that we can call join on it. 
+// It takes all the arguments given to run and treats them as the lines of a program.
 function run() {
   var env = Object.create(topEnv);
   var program = Array.prototype.slice
     .call(arguments, 0).join("\n");
   return evaluate(parse(program), env);
 }
+
+// function construct "fun"
+specialForms["fun"] = function(args, env) {
+  if (!args.length)
+    throw new SyntaxError("Functions need a body");
+  function name(expr) {
+    if (expr.type != "word")
+      throw new SyntaxError("Arg names must be words");
+    return expr.name;
+  }
+  var argNames = args.slice(0, args.length - 1).map(name);
+  var body = args[args.length - 1];
+
+  return function() {
+    if (arguments.length != argNames.length)
+      throw new TypeError("Wrong number of arguments");
+    var localEnv = Object.create(env);
+    for (var i = 0; i < arguments.length; i++)
+      localEnv[argNames[i]] = arguments[i];
+    return evaluate(body, localEnv);
+  };
+};
 
 // program example with while
 run("do(define(total, 0),",
@@ -161,3 +257,25 @@ run("do(define(total, 0),",
     "   for(define(count, 0), <=(count, n), define(count, +(count, 1)),",
     "         do(define(total, +(total, count)))),",
     "   print(total))");
+
+run("do(print(sum_array(array(1,2,3))))");
+
+run("do(print(sum_array(array(1,\"a\",3))))");
+
+run("do(define(sum, fun(array,",
+    "     do(define(i, 0),",
+    "        define(sum, 0),",
+    "        while(<(i, length(array)),",
+    "          do(define(sum, +(sum, element(array, i))),",
+    "             define(i, +(i, 1)))),",
+    "        sum))),",
+    "   print(sum(array(1, 2, 3))))");
+// → 6
+
+console.log(parse("# hello\nx"));
+// → {type: "word", name: "x"}
+
+console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
